1bd01883eb3d77ba75ecfe064cb566e8517e708a
Nikhil Babu <nikhilbabu213@gmail.com>
2025-07-09 21:58:39 +0200

    Add header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import FeedbackForm from "./FeedBackForm/FeedBackForm";
 import FeedbackList from "./FeedBackForm/FeedBackList";
 import "./App.css";
 import { useState } from "react";
+import logo from './assets/Mercedes-Logo.png';
+
 
 function App() {
   const [refresh, setRefresh] = useState(false);
@@ -11,11 +13,20 @@ function App() {
   };
 
   return (
-    <div className="container">
-      <h1>Feedback Collector</h1>
-      <FeedbackForm onSubmit={handleSubmit} />
-      <FeedbackList key={refresh} />
-    </div>
+    <>
+      <header className="header">
+        <div className="header-content">
+          <img src={logo} alt="Mercedes-Benz Logo" className="logo" />
+          <span className="company-name">Mercedes-Benz</span>
+        </div>
+      </header>
+
+      <div className="container">
+        <h1>Feedback Collector</h1>
+        <FeedbackForm onSubmit={handleSubmit} />
+        <FeedbackList key={refresh} />
+      </div>
+    </>
   );
 }
 
