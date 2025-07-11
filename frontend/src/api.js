5b101db04ec636f8aa54c70909bd58aa53579432
Nikhil Babu <nikhilbabu213@gmail.com>
2025-07-11 14:45:04 +0200

    Update API_BASE

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = "http://127.0.0.1:8000"; // change this for Azure later
+const API_BASE = "https://feedback-portal-nikhil.azurewebsites.net"; // change this for Azure later
 
 export const submitFeedback = (feedback) =>
   axios.post(`${API_BASE}/feedback`, feedback);
